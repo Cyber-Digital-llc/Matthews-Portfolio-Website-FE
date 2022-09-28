35fd8fe31188999c648dee236d9067be580be006
Joe Hernandez <j.t.hernandez999@gmail.com>
2022-09-27 21:23:51 -0700

    roughed out navbar, header, and footer components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className='header' id='#home'>
+            <h1>Matthew Hernandez</h1>
+            <h2>FILM/GAME COMPOSER</h2>
+        </div>
+    );
+}
+
+export default Header;
