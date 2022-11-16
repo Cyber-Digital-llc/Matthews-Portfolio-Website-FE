f6015c2a735324df572d5ddc9670c4888cadb449
jthernandez999 <j.t.hernandez999@gmail.com>
2022-11-15 17:22:14 -0800

    removed unused code

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import About from "../components/About";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
@@ -13,48 +14,11 @@ export default function Home() {
             </Head>
 
             <main className={styles.main}>
-                <Header />
-
-                <div className={styles.grid}>
-                    <a href='https://nextjs.org/docs' className={styles.card}>
-                        <h2>Documentation &rarr;</h2>
-                        <p>
-                            Find in-depth information about Next.js features and
-                            API.
-                        </p>
-                    </a>
-
-                    <a href='https://nextjs.org/learn' className={styles.card}>
-                        <h2>Learn &rarr;</h2>
-                        <p>
-                            Learn about Next.js in an interactive course with
-                            quizzes!
-                        </p>
-                    </a>
-
-                    <a
-                        href='https://github.com/vercel/next.js/tree/canary/examples'
-                        className={styles.card}
-                    >
-                        <h2>Examples &rarr;</h2>
-                        <p>
-                            Discover and deploy boilerplate example Next.js
-                            projects.
-                        </p>
-                    </a>
-
-                    <a
-                        href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        className={styles.card}
-                    >
-                        <h2>Deploy &rarr;</h2>
-                        <p>
-                            Instantly deploy your Next.js site to a public URL
-                            with Vercel.
-                        </p>
-                    </a>
+                <div>
+                    <Header />
+                </div>
+                <div>
+                    <About />
                 </div>
             </main>
 
