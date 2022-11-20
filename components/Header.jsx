8852f8b8e7b79ec7e475731ab0b7c12d14976fc3
jthernandez999 <j.t.hernandez999@gmail.com>
2022-11-20 15:49:27 -0800

    centered name and fixed width and height

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,10 +4,10 @@ import Navbar from './Navbar'
 const Header = () => {
   return (
     <>
-      <div className=" bg-cover bg-center bg-headerBGI bg-fixed w-[100vw] h-[90vh] mx-auto  flex flex-wrap items-center justify-between">
+      <div className=" bg-cover bg-center bg-headerBGI bg-fixed w-[100vw] h-[90vh] mx-auto  flex items-center justify-center ">
         {/* <Navbar /> */}
-        <div>
-          <h1 className="text-8xl">Matthew Hernandez</h1>
+        <div className="flex-col ">
+          <h1 className="font-bold text-8xl ">Matthew Hernandez</h1>
           <h3 className="text-6xl">FILM/GAME COMPOSER</h3>
         </div>
       </div>
