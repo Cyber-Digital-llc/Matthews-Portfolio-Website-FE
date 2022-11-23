b64ae973072c2a3e11df86d0410b16a192d56087
jthernandez999 <j.t.hernandez999@gmail.com>
2022-11-22 21:20:53 -0800

    debugging sizing in tailwind about section

--- components/About.jsx
+++ components/About.jsx
@@ -4,9 +4,9 @@ import headShot from '../public/headshot1536x1536px.jpg'
 
 const About = () => {
   return (
-    <div className="flex h-max bg-slate-900 py-0 px-0 w-full items-center border-t-[2rem] border-t-red-900">
-      <div className="flex justify-end w-full">
-        <div className="flex w-full xs:w-full sm:w-full md:w-[50%] ">
+    <div className="flex h-[75vh] w-full bg-slate-900 py-0 px-0 items-center border-t-[2rem] border-t-red-900">
+      <div className="flex justify-end w-full h-[100%] xl:max-h-screen-xl xl:max-w-screen-xl">
+        <div className=" flex justify-end xl:w-[80%] ">
           <Image
             draggable={false}
             src={headShot}
@@ -17,24 +17,24 @@ const About = () => {
         </div>
       </div>
 
-      <div className="flex w-full justify-start">
-        <div className=" px-6 py-0 w-[100%] ">
-          <div className="w-[350px] xs:w-full sm:w-full md:w-full lg:w-[50%] xl:[50%]">
-            <h2 className="text-3xl py-0">About</h2>
-            <p className="text-6xl py-0">Matthew Hernandez</p>
-            <p className="py-3">
+      <div className="flex w-full xl:w-[100vw] ">
+        <div className="flex justify-start px-14 ">
+          <div className="w-[60%] ">
+            <h2 className="text-2xl  max-w-prose">About</h2>
+            <p className="text-5xl py-0 max-w-prose">Matthew Hernandez</p>
+            <p className="py-3 max-w-prose">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque
               veniam consequatur dolorum recusandae corrupti voluptatem,
               obcaecati enim consectetur quis autem ipsa quisquam accusantium
               dignissimos aliquid cum totam, porro in ut.
             </p>
-            <p className="py-3">
+            <p className="py-3 max-w-prose">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ex,
               voluptatibus accusantium rem cumque vitae non voluptas sapiente
               expedita quisquam totam dolorum culpa alias minima ducimus
               consequatur neque commodi accusamus?
             </p>
-            <p className="py-3">
+            <p className="py-3 maw-w-prose">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione.
             </p>
           </div>
--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 const Header = () => {
   return (
     <>
-      <div className=" bg-cover bg-center bg-headerBGI bg-fixed w-[100vw] h-[100vh] xl:h-[60vh] mx-auto flex items-center justify-center ">
+      <div className=" bg-cover bg-center bg-headerBGI bg-fixed md:w-[100vw] xl:h-[61.5vh]  mx-auto flex items-center justify-center ">
         <div className="flex-col ">
           <h1 className="font-bold text-8xl ">Matthew Hernandez</h1>
           <h3 className="text-6xl">FILM/GAME COMPOSER</h3>
