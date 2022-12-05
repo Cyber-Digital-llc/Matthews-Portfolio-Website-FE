013c686f5cf85189c262c3d4cf37faf22307cdb1
jthernandez999 <j.t.hernandez999@gmail.com>
2022-12-04 20:48:33 -0800

    updated next/js to latest

---
+++ pages/electronicPressKit.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+const ElectronicPressKit = () => {
+  return (
+    <div>
+      <Navbar />
+      <h2>ElectronicPressKit</h2>
+    </div>
+  )
+}
+
+export default ElectronicPressKit
