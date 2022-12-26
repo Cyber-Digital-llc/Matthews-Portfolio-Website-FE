eabe38b5eea0e224e82f2da7673f3886ffb97bbe
jthernandez999 <j.t.hernandez999@gmail.com>
2022-12-25 16:18:51 -0800

    added media queries for mobile responsiveness

--- components/About.jsx
+++ components/About.jsx
@@ -4,14 +4,14 @@ const About = () => {
   return (
     <div
       id="about"
-      className="flex-col xxs:flex-col xs:flex-col xl:flex xl:flex-row xl:w-full lg:flex lg:flex-row lg:h-max md:flex md:h-max sm:flex sm:flex-row sm:h-max py-0 px-0 w-full sm:items-center  border-t-[2rem] border-t-red-900"
+      className="flex-col align-middle xxs:flex-col xs:flex-col xl:flex xl:flex-row xl:w-full lg:flex lg:flex-row lg:h-max md:flex md:h-max sm:flex sm:flex-row sm:h-max py-0 px-0 w-full sm:items-center  border-t-[2rem] border-t-red-900"
     >
       <div className="w-full">
         <div className="flex justify-center w-full bg-headShotBGI h-[90vh] bg-cover bg-scroll mx-auto"></div>
       </div>
 
       <div className="flex w-full justify-center ">
-        <div className="flex justify-start px-14 ">
+        <div className="flex justify-center mx-auto px-0 lg:justify-start lg:px-14 md:justify-start md:px-14 xl:justify-start xl:px-14 ">
           <div className="w-[60%] ">
             <h2 className="text-2xl  max-w-prose">About</h2>
             <p className="text-5xl py-0 max-w-prose">Matthew Hernandez</p>
