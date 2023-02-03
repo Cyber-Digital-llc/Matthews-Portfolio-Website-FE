f8bd7248f04b15fe9a3b09eea5323b736c10a316
jthernandez999 <j.t.hernandez999@gmail.com>
2023-02-02 21:39:25 -0800

    added bg gradient to the nav menu items

--- components/GlobalNavbar.jsx
+++ components/GlobalNavbar.jsx
@@ -49,25 +49,26 @@ function GlobalNavBar() {
           : 'fixed w-full h-20 z-[60]  bg-cover bg-center bg-no-repeat'
       }
     >
-      <div className="flex py-7 px-5 text-base text-stone-400 z-20 fixed justify-start items-start h-full 2xl:px-4 max-w-[2000px] mx-auto">
-        <div className="max-w-5xl mx-auto px-4">
-          <div className="flex items-center justify-between h-16">
-            {items.map((item) => {
-              return (
-                <ul className="flex pb-3 px-6 " key={item.label}>
-                  <li
-                    className="flex-row hover:border-t-4 py-3 focus-within:text-white focus-within:border-t-4"
-                    key={item.label}
-                  >
-                    {/* focus:border-t-4  */}
-                    <Link href={item.linkURL}>{item.label}</Link>
-                  </li>
-                </ul>
-              )
-            })}
-          </div>
-        </div>
+      <div className="flex py-7 px-5 text-base text-slate-300 z-20 fixed  justify-start items-start h-full 2xl:px-4 max-w-[2000px] mx-auto">
+        {/* <div className="max-w-5xl mx-auto px-4"> */}
+        {/* <div className="flex w-full px-2 2xl:px-4 mx-auto items-center justify-between h-16"> */}
+        {items.map((item) => {
+          return (
+            <ul className="hidden lg:flex mr-2 pb-3 px-6 " key={item.label}>
+              <li
+                // className="flex-row hover:border-t-4 py-3 focus-within:text-white focus-within:border-t-4"
+                className="hover:border-t-4 py-3 focus-within:text-white focus-within:border-t-4 mx-2 text-md uppercase group hover:cursor-pointer font-myriad text-transparent bg-clip-text bg-gradient-to-br from-[#ffffff] to-[#7a8ea2] hover:text-[#ffffff] self-center "
+                key={item.label}
+              >
+                {/* focus:border-t-4  */}
+                <Link href={item.linkURL}>{item.label}</Link>
+              </li>
+            </ul>
+          )
+        })}
       </div>
+      {/* </div> */}
+      {/* </div> */}
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import ContactForm from "../components/ContactForm";
 
 export default function Home() {
     return (
-        <div className='w-full min-h-max container relative mx-auto px-0 py-0 max-w-[2000px]'>
+        <div className='w-full min-h-max relative mx-auto px-0 py-0 max-w-[2000px]'>
             <Head>
                 <title>Matthew Hernandez</title>
                 <meta name='description' content='Film & Game Composer' />
