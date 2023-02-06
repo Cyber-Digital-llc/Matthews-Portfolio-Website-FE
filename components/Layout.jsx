42ae63144a08e006b044b6550dc260fc0ff8701d
jthernandez999 <j.t.hernandez999@gmail.com>
2023-02-05 19:44:26 -0800

    wip footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,7 @@ import GlobalNavBar from './GlobalNavbar'
 
 export const Layout = ({ children }) => {
   return (
-    <div className="layout">
+    <div className="">
       <GlobalNavBar />
       <div className="p-6 mx-auto">{children}</div>
     </div>
