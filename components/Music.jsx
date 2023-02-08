54057b188c7cbd7a9db4e100494371d824ff9e16
jthernandez999 <j.t.hernandez999@gmail.com>
2023-02-07 19:33:57 -0800

    wip

--- components/Music.jsx
+++ components/Music.jsx
@@ -5,34 +5,37 @@ const Music = () => {
   return (
     <div
       id="music"
-      className="flex flex-col-reverse xxs:flex xxs:flex-col-reverse sm:flex sm:flex-col-reverse xl:flex xl:flex-row h-max bg-black-900 py-0 px-0 w-full items-center"
+      className="flex flex-col align-middle xxs:flex-col xs:flex-col xl:flex xl:flex-row xl:w-full lg:flex lg:flex-row lg:h-max md:flex md:h-max sm:flex sm:flex-row sm:h-max py-0 px-0 w-full sm:items-center border-t-[2rem] 
+      border-t-[rgb(63,10,10)]"
     >
       <div className="w-full">
-        <div className="flex justify-center w-full bg-musicBGI bg-fit bg-contain bg-scroll 2xl:bg-fixed xl:bg-fixed lg:bg-fixed md:bg-fixed mx-auto bg-no-repeat "></div>
+        <div className="flex justify-end w-full bg-fit bg-center bg-musicBGI bg-contain bg-fixed md:bg-contain xl:bg-contain lg:w-[893px] lg:bg-fixed lg:bg-fit lg:bg-contain lg:h-[893px] xl:w-[893px] xl:h-[893px] xl:min-h-[980px] xl:flex h-96 mx-auto bg-no-repeat  "></div>
       </div>
 
       <div className="flex justify-center w-full">
-        <div className=" py-14 mx-[10%]">
-          <p className="text-6xl py-2">MUSIC</p>
-          <h2 className="text-3xl py-2">RECENT WORK</h2>
-          <iframe
-            width="100%"
-            height="300"
-            scrolling="no"
-            frameborder="no"
-            allow="autoplay"
-            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/220146829&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-          ></iframe>
+        <div className=" flex justify-center mx-auto py-[3%] lg:justify-start lg:px-14 md:justify-start md:px-14 xl:justify-start xl:px-14 ">
+          <div className="w-[65%] ">
+            <p className="text-6xl py-2">MUSIC</p>
+            <h2 className="text-3xl py-2">RECENT WORK</h2>
+            <iframe
+              width="100%"
+              height="300"
+              scrolling="no"
+              frameborder="no"
+              allow="autoplay"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/220146829&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+            ></iframe>
 
-          <p className="py-3 max-w-prose">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ex,
-            voluptatibus accusantium rem cumque vitae non voluptas sapiente
-            expedita quisquam totam dolorum culpa alias minima ducimus
-            consequatur neque commodi accusamus?
-          </p>
-          <p className="py-3 max-w-prose">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione.
-          </p>
+            <p className="py-3 max-w-prose">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ex,
+              voluptatibus accusantium rem cumque vitae non voluptas sapiente
+              expedita quisquam totam dolorum culpa alias minima ducimus
+              consequatur neque commodi accusamus?
+            </p>
+            <p className="py-3 max-w-prose">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione.
+            </p>
+          </div>
         </div>
       </div>
     </div>
