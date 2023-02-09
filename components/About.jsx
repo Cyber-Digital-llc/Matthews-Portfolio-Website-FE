30c524dd8ec1a09a8bf66711189f85510197e0e7
jthernandez999 <j.t.hernandez999@gmail.com>
2023-02-08 20:31:00 -0800

    about wip

--- components/About.jsx
+++ components/About.jsx
@@ -4,15 +4,15 @@ const About = () => {
   return (
     <div
       id="about"
-      className="flex flex-col align-middle xxs:flex-col xs:flex-col xl:flex xl:flex-row xl:w-full lg:flex lg:flex-row lg:h-max md:flex md:h-max sm:flex sm:flex-row sm:h-max py-0 px-0 w-[100vw] sm:items-center border-t-[2rem] border-t-[rgba(63,10,10,0.98)]"
+      className="flex flex-col h-max align-middle xxs:flex-col xs:flex-col xl:flex xl:flex-row xl:w-full lg:flex lg:flex-row lg:h-max lg:w-full amd:flex md:h-max sm:flex sm:flex-row sm:h-max py-0 px-0 w-full sm:items-center border-t-[2rem] border-t-[rgba(63,10,10,0.98)]"
     >
-      <div className="flex w-full xl:flex md:w-[50vw] lg:w-[50vw] xl:w-[50vw] 2xl:w-[50vw]">
-        <div className="w-full lg:max-h-max flex justify-end xl:items-center bg-headShotBGI bg-cover bg-center md:bg-contain lg:w-full md:min-h-max md:flex md:justify-end lg:bg-fit lg:flex lg:flex-row lg:justify-end  lg:min-h-max xl:justify-end xl:w-[900px] xl:h-[900px] xl:min-h-[980px] xl:bg-cover xl:flex h-96 bg-scroll mx-auto bg-no-repeat 2xl:flex "></div>
+      <div className="flex w-full xl:flex md:w-[50vw] lg:max-w-full xl:w-[50vw] 2xl:w-[50vw]">
+        <div className="w-full lg:max-h-max flex justify-end xl:items-center bg-headShotBGI bg-cover bg-center md:bg-contain lg:w-full md:min-h-max md:flex md:justify-end lg:bg-fit lg:h-[600px] lg:flex lg:flex-row lg:justify-end  lg:min-h-max xl:justify-end xl:w-[900px] xl:h-[900px] xl:min-h-[900px] xl:bg-cover xl:flex h-96 bg-scroll mx-auto bg-no-repeat 2xl:flex "></div>
       </div>
 
       <div className="flex w-full justify-center md:w-[50vw] lg:w-[50vw] xl:w-[50vw] 2xl:w-[50vw] ">
         <div className="flex w-full justify-center mx-auto py-[3%] lg:justify-start md:justify-start xl:justify-start ">
-          <div className="w-full lg:mx-auto px-10 lg:px-2 xl:mx-8 xl:px-36 md:w-1/2 md:px-0 ">
+          <div className="w-full lg:mx-auto px-10 xl:mx-8 xl:px-5 2xl:px-10 md:w-1/2 md:px-0 ">
             <h2 className="text-2xl  max-w-prose">About</h2>
             <p className="text-5xl py-0 max-w-prose">Matthew Hernandez</p>
             <p className="py-3 max-w-prose">
