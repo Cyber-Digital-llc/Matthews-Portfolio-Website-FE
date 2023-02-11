fc8f7937195f4d14d88d25ed866331a62cc5e088
jthernandez999 <j.t.hernandez999@gmail.com>
2023-02-11 13:03:28 -0800

    debugging back to top button

--- pages/index.js
+++ pages/index.js
@@ -9,32 +9,34 @@ import Music from "../components/Music";
 import GlobalNavBar from "../components/GlobalNavbar";
 import ContactForm from "../components/ContactForm";
 import MobileNav from "../components/MobileNav";
+
 export default function Home() {
     // Get the button
-    let mybutton = window.document.getElementById("btn-back-to-top");
+    // let mybutton = document.getElementById("btn-back-to-top");
 
     // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-        scrollFunction();
-    };
+    // window.onscroll = function () {
+    //     scrollFunction();
+    // };
 
-    function scrollFunction() {
-        if (
-            window.document.body.scrollTop > 20 ||
-            window.document.documentElement.scrollTop > 20
-        ) {
-            mybutton.style.display = "block";
-        } else {
-            mybutton.style.display = "none";
-        }
-    }
+    // function scrollFunction() {
+    //     if (
+    //         document.body.scrollTop > 20 ||
+    //         document.documentElement.scrollTop > 20
+    //     ) {
+    //         mybutton.style.display = "block";
+    //     } else {
+    //         mybutton.style.display = "none";
+    //     }
+    // }
     // When the user clicks on the button, scroll to the top of the document
-    mybutton.addEventListener("click", backToTop);
+    // mybutton.addEventListener("click", backToTop);
 
-    function backToTop() {
-        window.document.body.scrollTop = 0;
-        window.document.documentElement.scrollTop = 0;
-    }
+    // function backToTop() {
+    //     document.body.scrollTop = 0;
+    //     document.documentElement.scrollTop = 0;
+    // }
+
     return (
         <div className=''>
             <Head>
@@ -53,29 +55,6 @@ export default function Home() {
                     <Music />
                     {/* <Contact /> */}
                     <ContactForm />
-                    {/* <!-- Back to top button --> */}
-                    <button
-                        type='button'
-                        data-mdb-ripple='true'
-                        data-mdb-ripple-color='light'
-                        class='inline-block p-3 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out hidden bottom-5 right-5 fixed'
-                        id='btn-back-to-top'
-                    >
-                        <svg
-                            aria-hidden='true'
-                            focusable='false'
-                            data-prefix='fas'
-                            class='w-4 h-4'
-                            role='img'
-                            xmlns='http://www.w3.org/2000/svg'
-                            viewBox='0 0 448 512'
-                        >
-                            <path
-                                fill='currentColor'
-                                d='M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z'
-                            ></path>
-                        </svg>
-                    </button>
                 </div>
             </main>
 
@@ -91,6 +70,28 @@ export default function Home() {
                     </span>
                 </a>
             </footer>
+            <button
+                type='button'
+                data-mdb-ripple='true'
+                data-mdb-ripple-color='light'
+                class='inline-block p-3 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out bottom-5 right-5'
+                id='btn-back-to-top'
+            >
+                <svg
+                    aria-hidden='true'
+                    focusable='false'
+                    data-prefix='fas'
+                    class='w-4 h-4'
+                    role='img'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 448 512'
+                >
+                    <path
+                        fill='currentColor'
+                        d='M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z'
+                    ></path>
+                </svg>
+            </button>
         </div>
     );
 }
