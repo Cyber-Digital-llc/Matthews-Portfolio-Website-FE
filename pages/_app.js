7716245135d48339d4dccb6fe2b850b639687eaa
jthernandez999 <j.t.hernandez999@gmail.com>
2023-02-16 19:37:30 -0800

    fixed footer not showing up on mobile

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
     return (
         <div className='relative'>
-            <div className='absolute z-[1] w-screen h-screen overflow-clip'>
-                <div className='absolute z-[1] h-full aspect-[16/9]'>
+            <div className='absolute  w-screen h-screen overflow-clip'>
+                <div className='absolute  h-full '>
                     <img
                         src={"/heroBGcopy.jpg"}
                         className='object-none object-center h-full fixed'
